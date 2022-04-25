a07bc9afc12912c80157f60cd37d7974531c00e0
ovchie <fast.rem1ck3@gmail.com>
2022-04-26 00:29:48 +0300

    initial

---
+++ conf.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once __DIR__ . '/modules/index.php';
+require_once __DIR__ . '/db_conf.php';
+
+
+define('ROOT', $_SERVER['DOCUMENT_ROOT']);
+define('CFRS_TIME', 3600);
+define('SALT', 'LawJ3SmfsUBTdRS9kNUVV94wkHQ56x');
---
+++ view/pages/index.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Social market</title>
+    <link rel="stylesheet" href="/view/assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/view/assets/css/style.css">
+</head>
+
+<body>
+    <div class="header-wrapper">
+        <nav class="navbar navbar-expand-md navbar-light header">
+            <div class="container-fluid container-md header-frame">
+                <a class="navbar-brand" href="/">Social market</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">email@mail.com</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">@telegramm</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">+79876543211</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">vk-link</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </div>
+
+    <div class="container-md main">
+        <div class="row row-cols-1 row-cols-md-3 g-4">
+            <div class="col">
+                <div class="card">
+                    <img src="/view/assets/images/razer-toaster_01.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
+                            additional content. This content is a little bit longer.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card">
+                    <img src="/view/assets/images/razer-toaster_01.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
+                            additional content. This content is a little bit longer.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card">
+                    <img src="/view/assets/images/razer-toaster_01.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
+                            additional content.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card">
+                    <img src="/view/assets/images/razer-toaster_01.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
+                            additional content. This content is a little bit longer.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card">
+                    <img src="/view/assets/images/razer-toaster_01.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
+                            additional content. This content is a little bit longer.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+<script src="/view/assets/js/jquery-3.6.0.min.js"></script>
+<script src="/view/assets/js/bootstrap.min.js"></script>
+<script src="/view/assets/js/submit_cell.js"></script>
+
+</html>
