a67b09c1174005abaea70a5201d357e37dfd29d5
ovchie <ov4innikovsa@yandex.ru>
2024-03-08 13:08:37 +0300

    docker

--- src/modules/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require_once 'based.php';
-require_once 'categories.php';
-require_once 'db.php';
-require_once 'item.php';
-require_once 'session.php';
--- src/view/pages/adm/pages/exit.php
+++
@@ -1,4 +0,0 @@
-<?php 
-
-session_clear_value('admin');
-redirect('/');
